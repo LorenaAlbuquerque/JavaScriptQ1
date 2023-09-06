7846d677c0409dcaa7c9f7c328966d9e07708746
LorenaAlbuquerque <92889078+LorenaAlbuquerque@users.noreply.github.com>
2023-09-06 09:40:02 -0300

    Add files via upload

---
+++ ListaJSQ1/script.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = 5;
+var c = 1;
+var d;
+
+c = b--
+document.write("a:",a, "<br> b:", b, "<br> c:", c, "<br> d:", d, "<br><br>");
+
+b = a++
+document.write("a:",a, "<br> b:", b, "<br> c:", c, "<br> d:", d, "<br><br>");
+
+a =  5  + "1"
+document.write("a:",a, "<br> b:", b, "<br> c:", c, "<br> d:", d, "<br><br>");
+
+d = "1" + "1"
+document.write("a:",a, "<br> b:", b, "<br> c:", c, "<br> d:", d, "<br><br>");
+
+
